2bda623e1f6ab8716acfec8377539c3964c0c238
Ivan Popov <popovbm@gmail.com>
2023-07-12 22:44:20 +0300

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'vendor/akaunting/laravel-language/src/Resources/assets/img/flags/',
             ],
             refresh: true,
         }),
